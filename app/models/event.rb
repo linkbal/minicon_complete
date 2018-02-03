5ac2ee898beceeee38b5f383df0386722aad7cb6
Dennis Tanaka <me@denakitan.com>
2018-02-03 04:29:17 +0000

    Add validators to the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,13 @@
 class Event < ApplicationRecord
   belongs_to :event_category
+  
+  validates :event_category, :title, :location, :start_at, :ticket_price, :ticket_quantity, presence: true
+  validates :event_category, :title, :location, :start_at, :ticket_price, :ticket_quantity, presence: true
+  validates :title, length: { maximum: 80 }, uniqueness: true
+  validates :location, length: { maximum: 20 }
+  validates :ticket_price, :ticket_quantity, numericality: { only_integer: true, greater_than: 0 }
+    
+  validates_each :start_at do |record, attr, value|
+    record.errors.add(attr, 'must be at least a week from now') if value < Time.current + 1.week
+  end
 end
