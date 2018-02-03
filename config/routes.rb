9de5bf16d20baf502ef2b6ddaf4f3d0805d6a069
Dennis Tanaka <me@denakitan.com>
2018-02-03 08:45:48 +0000

    Add the events controller and make preparations to add dynamic content

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'controller_name#action_name'
-  root 'application#hello'
+  root 'events#index'
   
   namespace :admin do
     resources :event_categories
