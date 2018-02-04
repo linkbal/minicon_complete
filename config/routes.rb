7aa9eecb5ebf69edf696d9560d1b8a25d8a33eda
Dennis Tanaka <me@denakitan.com>
2018-02-04 13:19:08 +0000

    Add user authentication with the Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'controller_name#action_name'
   root 'events#index'
