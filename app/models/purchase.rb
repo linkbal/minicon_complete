ffd8b211a1d636aa0a9614afa1b725ac399c94fb
Dennis Tanaka <me@denakitan.com>
2018-02-14 15:43:39 +0000

    Add sold out validation for purchases

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,8 +2,14 @@ class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
+  enum status: [:complete, :cancelled]
+
   validates :user, :event, :status, :ticket_quantity, :total_price, presence: true
   validates :ticket_quantity, :total_price, numericality: { only_integer: true, greater_than: 0 }
+  validate :ticket_quantity_enough
 
-  enum status: [:complete, :cancelled]
+  def ticket_quantity_enough
+    active_purchases = Purchase.complete.joins(:event).where(events: { id: event.id })
+    errors.add(:base, 'This event is sold out.') if active_purchases.count + ticket_quantity > event.ticket_quantity
+  end
 end
