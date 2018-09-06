1d8558c95eb74030b1ce79972dfb9abed6b0d929
Dennis Tanaka <me@denakitan.com>
2018-09-06 04:28:08 +0000

    Update seed file dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ EventCategory.create!([
   {name: "Gourmet", description: "Gourmet themed events"}
 ])
 Event.create!([
-  {title: "Nice Konkatsu Event", location: "銀座", start_at: "2018-03-28 09:00:00", ticket_price: 5000, ticket_quantity: 30, event_category_id: 1},
-  {title: "Great Koikatsu Event", location: "渋谷", start_at: "2018-04-05 09:00:00", ticket_price: 4000, ticket_quantity: 25, event_category_id: 2},
-  {title: "Gourmet Themed Event", location: "渋谷", start_at: "2018-03-13 14:36:00", ticket_price: 6000, ticket_quantity: 35, event_category_id: 3}
+  {title: "Nice Konkatsu Event", location: "銀座", start_at: "2020-03-28 09:00:00", ticket_price: 5000, ticket_quantity: 30, event_category_id: 1},
+  {title: "Great Koikatsu Event", location: "渋谷", start_at: "2020-04-05 09:00:00", ticket_price: 4000, ticket_quantity: 25, event_category_id: 2},
+  {title: "Gourmet Themed Event", location: "渋谷", start_at: "2020-03-13 14:36:00", ticket_price: 6000, ticket_quantity: 35, event_category_id: 3}
 ])
